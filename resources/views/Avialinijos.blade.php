ae032e53b38b633674a38a650cb1005b85de4c76
VikUrn <viktoras.urna@gmail.com>
2022-11-04 10:50:34 +0200

    hi

--- resources/views/Avialinijos.blade.php
+++ resources/views/Avialinijos.blade.php
@@ -9,7 +9,7 @@
 
 <body style="color:#EEEEEE" class="bg-primary" >
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
-	<nav style="background-color:#222831" class="navbar navbar-expand-lg">
+	<nav style="background-color:#f5fd00ff" class="navbar navbar-expand-lg">
 		<div class="container-fluid" >
 			<a class="navbar-brand"></a>
 			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
